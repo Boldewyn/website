7e24d8bdec1e1ec7100c97f6afd224f2a42c3998
Manuel Strehl <git@manuel-strehl.de>
2010-11-06 22:56:14 +0100

    towards full site

--- _categories.py
+++ _categories.py
@@ -1,6 +1,7 @@
 """"""
 
 
+import os
 from _templates import render_template
 
 
@@ -8,12 +9,15 @@ def render_tags(target, articles):
     """"""
     tags = {}
     for article in articles:
-        a_tags = getattr(article.headers, 'SUBJECT', [])
+        a_tags = article.headers.get('SUBJECT', [])
         for a_tag in a_tags:
             if a_tag not in tags:
                 tags[a_tag] = []
             tags[a_tag].append(article)
     for tag, a in tags.iteritems():
+        description = None
+        if os.path.exists("_doc/%s.tag.html" % tag):
+            description = open("_doc/%s.tag.html" % tag).read()
         render_template("tag", target+"/tag/"+
                         tag+"/index.html", **locals())
 
@@ -38,8 +42,16 @@ def render_indexes(target, articles):
         if article.category not in categories:
             categories[article.category] = []
         categories[article.category].append(article)
+        if "/" in article.category:
+            rootcat = article.category.split("/")[0]
+            if rootcat not in categories:
+                categories[rootcat] = []
+            categories[rootcat].append(article)
     for category, a in categories.iteritems():
         if category+"/index.html" not in a:
+            description = None
+            if os.path.exists("_doc/%s.category.html" % category):
+                description = open("_doc/%s.category.html" % category).read()
             render_template("category", target+"/"+
                             category+"/index.html", **locals())
 
--- _templates.py
+++ _templates.py
@@ -9,17 +9,21 @@ from _settings import settings
 
 
 _dir = os.path.dirname(__file__)
-_lookup = TemplateLookup(directories=["."])
+_lookup = TemplateLookup(directories=["."], default_filters=["x"])
 
 
 def render_template(template, path, **ctx):
     """"""
     ctx['settings'] = settings
+    if "articles" in ctx and "categories" not in ctx:
+        ctx["categories"] = get_categories(ctx["articles"])
     if "articles" in ctx and "tagcloud" not in ctx:
         ctx["tagcloud"] = get_tagcloud(ctx["articles"])
+    if "articles" in ctx and "archives" not in ctx:
+        ctx["archives"] = get_archives(ctx["articles"])
     path = os.path.abspath(path)
     tpl = Template(filename="_templates/"+template+'.mako',
-                   lookup=_lookup)
+                   lookup=_lookup, default_filters=["x"])
     if not os.path.isdir(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
     to = open(path, 'w')
@@ -32,7 +36,7 @@ def render_template(template, path, **ctx):
 
 
 def get_tagcloud(articles, offset=1):
-    """"""
+    """Get all tags from the categories"""
     tags = {}
     for article in articles:
         for tag in article.headers['SUBJECT']:
@@ -45,4 +49,28 @@ def get_tagcloud(articles, offset=1):
             if n < offset:
                 del tags[tag]
     return tags
+
+
+def get_categories(articles):
+    """Get all categories from the articles"""
+    categories = []
+    for article in articles:
+        if article.category not in categories:
+            categories.append(article.category)
+        if "/" in article.category:
+            rootcat = article.category.split("/")[0]
+            if rootcat not in categories:
+                categories.append(rootcat)
+    categories.sort()
+    return categories
+
+
+def get_archives(articles):
+    """Get all archive links"""
+    dates = []
+    for article in articles:
+        d = article.headers['DATE'].strftime("%Y/%m")
+        if d not in dates:
+            dates.append(d)
+    return dates
 
