b69399302a1bbdf4e6a83ec70911e951920e4d4e
Manuel Strehl <git@manuel-strehl.de>
2011-03-21 16:48:58 +0100

    fix quoting: dont use "+" in path part of URLs

--- _webtools/templatedefs.py
+++ _webtools/templatedefs.py
@@ -15,7 +15,7 @@ from babel import UnknownLocaleError
 def urlquote(string):
     """URL quoting without the slash"""
     string = string.encode("utf8")
-    return urllib.quote_plus(string, "/")
+    return urllib.quote(string, "/")
 
 
 def aa(path):
