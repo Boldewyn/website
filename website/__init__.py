86841510463c67fe90c9c74bdb36bc88beed78d7
Manuel Strehl <git@manuel-strehl.de>
2011-07-13 21:58:25 +0200

    v1.1

--- website/__init__.py
+++ website/__init__.py
@@ -7,7 +7,7 @@ logger = logging.getLogger("website")
 logger.addHandler(logging.StreamHandler())
 
 
-VERSION = (1, 0)
+VERSION = (1, 1)
 
 def get_version():
     return ".".join(map(lambda i: str(i), VERSION))
