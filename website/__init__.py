1653f40e426739cbe96e1fe0c24417d30b5b4dc7
Manuel Strehl <git@manuel-strehl.de>
2011-07-25 15:53:59 +0200

    add logger config and formatter

--- website/__init__.py
+++ website/__init__.py
@@ -4,7 +4,10 @@ import logging
 
 
 logger = logging.getLogger("website")
-logger.addHandler(logging.StreamHandler())
+_handler = logging.StreamHandler()
+_formatter = logging.Formatter("[%(levelname)s] %(message)s - %(name)s")
+_handler.setFormatter(_formatter)
+logger.addHandler(_handler)
 
 
 VERSION = (1, 1)
