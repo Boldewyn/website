10ae2d630271da8bdb0fc6218b412f74fb12c7ba
Manuel Strehl <git@manuel-strehl.de>
2011-08-17 20:49:12 +0200

    test archive and hard language

--- tests/testWebsite.py
+++ tests/testWebsite.py
@@ -4,6 +4,7 @@ import unittest
 import os
 import tempfile
 import shutil
+from datetime import datetime
 from website._webtools.bootstrap import bootstrap
 from website._webtools.build import build
 from website._webtools import templatedefs
@@ -31,17 +32,20 @@ class BootstrapTestCase(unittest.TestCase):
         """Bootstrap in an existing non-empty target"""
         os.mkdir(os.path.join(self.tmpdir, "nonempty_target"))
         open(os.path.join(self.tmpdir, "nonempty_target/x"), 'w').close()
-        self.assertRaises(ValueError, bootstrap, os.path.join(self.tmpdir, "nonempty_target"), {})
+        self.assertRaises(ValueError, bootstrap,
+                          os.path.join(self.tmpdir, "nonempty_target"), {})
 
     def test_noconf(self):
         """Bootstrap with empty config"""
         bootstrap(os.path.join(self.tmpdir, "noconf"), None)
-        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir, "noconf/_config.py")), "config file not added")
+        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir,
+                        "noconf/_config.py")), "config file not added")
 
     def test_minimal(self):
         """Bootstrap with a minimal config"""
         bootstrap(os.path.join(self.tmpdir, "minimal"), self.baseConfig)
-        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir, "minimal/_config.py")), "config file not added")
+        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir,
+                        "minimal/_config.py")), "config file not added")
 
     def test_usual(self):
         """Test with all usual config options set"""
@@ -50,7 +54,8 @@ class BootstrapTestCase(unittest.TestCase):
         cfg["LANGUAGE"] = "en"
         cfg["DISQUS_NAME"] = "__test__"
         bootstrap(os.path.join(self.tmpdir, "usual"), cfg)
-        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir, "usual/_config.py")), "config file not added")
+        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir,
+                        "usual/_config.py")), "config file not added")
 
     def test_in_docroot(self):
         """Bootstrap with URL path set to '/'"""
@@ -61,8 +66,10 @@ class BootstrapTestCase(unittest.TestCase):
               "AUTHOR": "John Doe",
           }
         })
-        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir, "in_docroot/robots.txt")), "robots.txt not added")
-        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir, "in_docroot/humans.txt")), "humans.txt not added")
+        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir,
+                        "in_docroot/robots.txt")), "robots.txt not added")
+        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir,
+                        "in_docroot/humans.txt")), "humans.txt not added")
 
     def test_below_docroot(self):
         """Bootstrap with URL path set to '/foo/'"""
@@ -73,8 +80,10 @@ class BootstrapTestCase(unittest.TestCase):
               "AUTHOR": "John Doe",
           }
         })
-        self.assertFalse(os.path.isfile(os.path.join(self.tmpdir, "below_docroot/robots.txt")), "robots.txt not added")
-        self.assertFalse(os.path.isfile(os.path.join(self.tmpdir, "below_docroot/humans.txt")), "humans.txt not added")
+        self.assertFalse(os.path.isfile(os.path.join(self.tmpdir,
+                         "below_docroot/robots.txt")), "robots.txt not added")
+        self.assertFalse(os.path.isfile(os.path.join(self.tmpdir,
+                         "below_docroot/humans.txt")), "humans.txt not added")
 
     def tearDown(self):
         shutil.rmtree(self.tmpdir)
@@ -97,19 +106,32 @@ class BuildTestCase(unittest.TestCase):
 
     def test_initial_build(self):
         """Check, if building directly after bootstrap works"""
-        build()
+        build({"BUILD_TARGET": os.path.join(self.tmpdir, "site")})
 
     def test_moved_build(self):
         """Check, if building to a different directory works"""
         builddir = tempfile.mkdtemp()
         build({"BUILD_TARGET": builddir})
-        self.assertTrue(os.path.isfile(os.path.join(builddir, "humans.txt")), "humans.txt not added to non-default build")
+        self.assertTrue(os.path.isfile(os.path.join(builddir, "humans.txt")),
+                        "humans.txt not added to non-default build")
 
     def test_empty_build(self):
         """Check, if building w/o articles works"""
         shutil.rmtree(os.path.join(self.tmpdir, "_articles"))
         os.mkdir(os.path.join(self.tmpdir, "_articles"))
-        build()
+        build({"BUILD_TARGET": os.path.join(self.tmpdir, "site")})
+
+    def test_archive(self):
+        """Check, if an archive is built"""
+        build({"BUILD_TARGET": os.path.join(self.tmpdir, "site")})
+        self.assertTrue(os.path.isdir(os.path.join(self.tmpdir,
+                        "site/archive")), "archive directory not built")
+        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir,
+                        "site/archive/%s/index.html" % datetime.now().year)),
+                        "archive/year directory not built")
+        self.assertTrue(os.path.isfile(os.path.join(self.tmpdir,
+                        "site/archive/%s/%02d/index.html" % (datetime.now().year,
+                        datetime.now().month))), "archive/year/month not built")
 
     def tearDown(self):
         os.chdir(self.cwd)
@@ -134,12 +156,11 @@ class ArticleTestCase(unittest.TestCase):
     def test_root_article(self):
         """Test, that an article in the site root is built"""
         art = open("_articles/foo.html", "w")
-        art.write("""Title: Foo
-
-<p>Test</p>""")
+        art.write("Title: Foo\n\n<p>Test</p>")
         art.close()
         build({"BUILD_TARGET": os.path.join(self.tmpdir, "site")})
-        self.assertTrue(os.path.isfile("site/foo.html"), "article not built")
+        self.assertTrue(os.path.isfile("site/foo.html"),
+                        "article not built")
         c = open("site/foo.html")
         self.assertIn("Foo", c.read(), "article content wrong")
         c.close()
@@ -148,15 +169,42 @@ class ArticleTestCase(unittest.TestCase):
         """Test, that an article in a folder is built"""
         os.mkdir("_articles/bar")
         art = open("_articles/bar/foo.html", "w")
-        art.write("""Title: Foo
-
-<p>Test</p>""")
+        art.write("Title: Foo\n\n<p>Test</p>")
         art.close()
         build({"BUILD_TARGET": os.path.join(self.tmpdir, "site")})
-        self.assertTrue(os.path.isfile("site/bar/foo.html"), "article not built")
-        self.assertTrue(os.path.isfile("site/bar/index.html"), "category index page not built")
+        self.assertTrue(os.path.isfile("site/bar/foo.html"),
+                        "article not built")
+        self.assertTrue(os.path.isfile("site/bar/index.html"),
+                        "category index page not built")
         c = open("site/bar/foo.html")
         self.assertIn("Foo", c.read(), "article content wrong")
+        c.close()
+
+    def test_minimal_article(self):
+        """Test, that a minimal article is built"""
+        art = open("_articles/foo.html", "w")
+        art.write("\n\n")
+        art.close()
+        build({"BUILD_TARGET": os.path.join(self.tmpdir, "site")})
+        self.assertTrue(os.path.isfile("site/foo.html"),
+                        "article not built")
+        c = open("site/foo.html")
+        self.assertIn("<title>", c.read(), "article content wrong")
+        c.close()
+
+    def test_hard_language(self):
+        """Test, that an article with language in URL is built"""
+        art = open("_articles/foo.en.html", "w")
+        art.write("Title: foo\n\n")
+        art.close()
+        build({
+            "BUILD_TARGET": os.path.join(self.tmpdir, "site"),
+            "NEGOTIATE_EXTENSIONS": True,
+        })
+        self.assertTrue(os.path.isfile("site/foo.en.html"),
+                        "article not built")
+        c = open("site/foo.en.html")
+        self.assertIn('xml:lang="en"', c.read(), "article content wrong")
         c.close()
 
     def tearDown(self):
